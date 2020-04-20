119c3af25193c6a4af345ed3aa1ec826e7af5b1e
anchaal1104 <30515566+anchaal1104@users.noreply.github.com>
2020-04-20 15:16:21 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,16 @@
+let sum=function(no1,no2)
+{
+let added=no1+no2
+return added
+}
+
+let diff=function(no1,no2)
+{
+let dif=no2-no1
+return dif
+}
+
+add=sum(1020,3022)
+console.log(`sum is ${add}`)
+differ=diff(1020,3022)
+console.log(`Difference is ${differ}`)
